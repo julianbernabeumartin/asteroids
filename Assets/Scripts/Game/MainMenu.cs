bce80e99be5ae9ebfac45fc9ab42d50b0d2ee1c7
JulianBernabeu <103448284+JulianBernabeu@users.noreply.github.com>
2023-04-06 19:18:19 -0300

    Final changes

--- Assets/Scripts/Game/MainMenu.cs
+++ Assets/Scripts/Game/MainMenu.cs
@@ -7,4 +7,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Game", LoadSceneMode.Single);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
